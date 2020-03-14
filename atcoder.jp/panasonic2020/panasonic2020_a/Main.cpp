ed405e1a9384e0e93cd1c19b285d52b9c166a01f
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-03-14 21:01:55 +0900

    [AC] panasonic2020 panasonic2020_a

---
+++ atcoder.jp/panasonic2020/panasonic2020_a/Main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll MOD = 1000000007;
+
+int main() {
+    int n; cin >> n;
+    int a[32] = {1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51};
+    cout << a[n-1] << endl;
+}   
