0c9e10ab747ccfdeeab100127e818d52068b96c8
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-03-24 13:04:39 +0900

    [AC] abc014 abc014_1

---
+++ atcoder.jp/abc014/abc014_1/Main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll MOD = 1000000007;
+const long long INF = 1LL << 60;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+int main()
+{   
+    int n,m; cin >> n >> m;
+    int i=0;
+    while(1){
+        if((n+i)%m==0){
+            cout << i << endl;
+            break;
+        }
+        i++;
+    }
+}
+    
