4f48cd643aeae6dd83ce4caa7b9a1ac4251b2095
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-03-27 22:56:08 +0900

    [AC] abc103 abc103_c

---
+++ atcoder.jp/abc103/abc103_c/Main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(), (v).end()
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<n;++i)
+ll MOD = 1000000007;
+const long long INF = 1LL << 60;
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+
+int main()
+{   
+    int n; cin >> n;
+    vector<ll> a(n);
+    rep(i,n)cin >> a[i];
+    ll res = 0;
+    rep(i,n){
+        res += a[i]-1;
+    }
+    cout << res << endl;
+}
