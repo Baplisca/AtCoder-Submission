33379bee4f4bc7ab901e046f038fee9ebe42e91f
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-03-28 16:53:48 +0900

    [AC] abc148 abc148_c

---
+++ atcoder.jp/abc148/abc148_c/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(v) (v).begin(), (v).end()
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<n;++i)
+ll MOD = 1000000007;
+const long long INF = 1LL << 60;
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+
+int main()
+{   
+    ll a,b; cin >> a >> b;
+    cout << a/__gcd(a,b)*b << endl;
+}
