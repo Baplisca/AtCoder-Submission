2b6ac80488b5bb11b408b143f2eb6e55da97ccf8
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-03-29 16:39:55 +0900

    [AC] abc148 abc148_a

---
+++ atcoder.jp/abc148/abc148_a/Main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//#define int long long
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<n;++i)
+#define gearup ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef pair<ll, ll> P;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+ll MOD = 1000000007;
+const long long INF = 1LL << 60;
+//cout<<fixed<<setprecision(10);
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+template<class T> void debug(T v){rep(i,v.size()) cout<<v[i]<<" ";cout<<endl;}
+
+signed main()
+{   
+    gearup;
+    int a,b; cin >> a >> b;
+    cout << 6 -a -b << endl;
+}
