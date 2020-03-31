0b65e5ab18563574a71df2d4a7bdbf2e485c0db9
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-03-31 15:19:05 +0900

    [AC] agc023 agc023_a

---
+++ atcoder.jp/agc023/agc023_a/Main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//#define int long long
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<n;++i)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define EPS (1e-7)
+#define gearup ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef pair<ll, ll> P;
+typedef vector<ll> vl;
+typedef vector<vector<ll> > vvl;
+typedef vector<vector<vector<ll> > > vvvl;
+ll MOD = 1000000007;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+template<class T> void debug(T v){rep(i,v.size()) cout<<v[i]<<" ";cout<<endl;}
+
+signed main()
+{   
+    gearup;
+    int n; cin >> n;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    vector<ll> s(n+1,0);
+    rep(i,n) s[i+1] = s[i] + a[i];
+    map<ll,ll> mp;
+    rep(i,n+1) mp[s[i]]++;
+    ll res = 0;
+    for(auto a:mp){
+        res += a.second *(a.second-1)/2;
+    }
+    cout << res << endl;
+}
