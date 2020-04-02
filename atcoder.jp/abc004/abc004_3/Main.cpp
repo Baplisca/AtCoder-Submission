d271308ea6da38fdc1c74661d0680f3f3ca6e00a
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-04-02 19:24:26 +0900

    [AC] abc004 abc004_3

---
+++ atcoder.jp/abc004/abc004_3/Main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//#define int long long
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<n;++i)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define EPS (1e-7)
+#define gearup ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef pair<ll, ll> P;
+typedef vector<ll> vl;
+typedef vector<vector<ll> > vvl;
+typedef vector<vector<vector<ll> > > vvvl;
+ll MOD = 1000000007;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+template<class T> void debug(T v){rep(i,v.size()) cout<<v[i]<<" ";cout<<endl;}
+
+signed main()
+{   
+    gearup;
+    ll n; cin >> n;
+    n %= 30;
+    vector< int> a(6);
+    rep(i,6) a[i] = i+1;
+    rep(i,n){
+        int j = (i%5);
+        swap(a[j],a[j+1]);
+    }
+    rep(i,6)cout << a[i];
+    cout << endl;
+}
