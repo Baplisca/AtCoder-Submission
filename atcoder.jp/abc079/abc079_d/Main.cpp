b139630af2bcde3fed256fed7a51581d3320ae71
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-04-09 21:14:52 +0900

    [AC] abc079 abc079_d

---
+++ atcoder.jp/abc079/abc079_d/Main.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//#define int long long
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<n;++i)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define EPS (1e-7)
+#define gearup ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef pair<ll, ll> P;
+typedef vector<ll> vl;
+typedef vector<vector<ll> > vvl;
+typedef vector<vector<vector<ll> > > vvvl;
+ll MOD = 1000000007;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+template<class T> void debug(T v){rep(i,v.size()) cout<<v[i]<<" ";cout<<endl;}
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+signed main()
+{   
+    gearup;
+    int h,w; cin >> h >> w;
+    vvl c(10,vl(10));
+    rep(i,10) rep(j,10) cin >> c[i][j];
+    vvl a(h,vl(w));
+    rep(i,h) rep(j,w) cin >> a[i][j];
+    
+    //ワーシャルフロイド法
+    int n=10;
+    for(int k=0;k<n;k++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                chmin(c[i][j],c[i][k]+c[k][j]);
+            }
+        }
+    }
+
+    ll res = 0;
+    rep(i,h){
+        rep(j,w){
+            if(a[i][j] == -1)continue;
+            res += c[a[i][j]][1];
+        }
+    }
+    cout << res << endl;
+}
