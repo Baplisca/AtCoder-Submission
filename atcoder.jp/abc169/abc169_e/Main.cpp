baeadfecf0681b728128f9cf073e01597d47638a
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-06-01 23:22:43 +0900

    [AC] abc169 abc169_e

---
+++ atcoder.jp/abc169/abc169_e/Main.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//#define int long long
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<n;++i)
+#define exrep(i, a, b) for(ll i = a; i < b; i++)
+#define out(x) cout << x << endl
+#define EPS (1e-7)
+#define gearup ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef long double ld;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pair<int,int> > vpii;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef vector<vector<bool> > vvb;
+typedef vector<vector<double> > vvd;
+typedef vector<vector<string> > vvs;
+typedef vector<ll> vl;
+typedef vector<vector<ll> > vvl;
+typedef vector<vector<vector<ll> > > vvvl;
+ll MOD = 1000000007;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+template<class T> void debug(T v){rep(i,v.size()) cout<<v[i]<<" ";cout<<endl;}
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+signed main()
+{   
+    gearup;
+    ll n; cin >> n;
+    vl a(n),b(n);
+    vpii p;
+    rep(i,n){
+      cin >> a[i] >> b[i];
+      p.push_back({a[i],b[i]});
+    }
+    auto c = a;
+    auto d = b;
+    sort(all(p));
+    sort(all(c));
+    sort(all(d));
+    ll res = 0;
+    if(n & 1){
+      ll left = c[n/2];
+      ll right = d[n/2];
+      res = right - left + 1;
+    }
+    else{
+      ll left1 = c[n/2 - 1];
+      ll left2 = c[n/2];
+      ll right1 = d[n/2 - 1];
+      ll right2 = d[n/2];
+
+      ll left = left1+left2;
+      ll right = right1+right2;
+      res = right - left + 1;
+    }
+    out(res);
+}
