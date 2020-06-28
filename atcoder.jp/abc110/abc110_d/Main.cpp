cebff6d3f1120df9083671cf06c53eaec033de49
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-06-28 22:25:51 +0900

    [AC] abc110 abc110_d

---
+++ atcoder.jp/abc110/abc110_d/Main.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//#define int long long
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<n;++i)
+#define exrep(i, a, b) for(ll i = a; i < b; i++)
+#define out(x) cout << x << endl
+#define EPS (1e-7)
+#define gearup ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pair<int,int> > vpii;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef vector<vector<bool> > vvb;
+typedef vector<vector<double> > vvd;
+typedef vector<vector<string> > vvs;
+typedef vector<ll> vl;
+typedef vector<vector<ll> > vvl;
+typedef vector<vector<vector<ll> > > vvvl;
+ll MOD = 1000000007;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+template<class T> void debug(T v){rep(i,v.size()) cout<<v[i]<<" ";cout<<endl;}
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//以降 cin の入力元が 'input.txt' になる
+//std::ifstream in("input.txt");
+//std::cin.rdbuf(in.rdbuf());
+
+struct mint {
+    long long x;
+    mint(long long x=0):x((x%MOD+MOD)%MOD){}
+    mint operator-() const { return mint(-x);}
+    mint& operator+=(const mint a) {
+        if ((x += a.x) >= MOD) x -= MOD;
+        return *this;
+    }
+    mint& operator-=(const mint a) {
+        if ((x += MOD-a.x) >= MOD) x -= MOD;
+    	return *this;
+    }
+    mint& operator*=(const mint a) {
+    	(x *= a.x) %= MOD;
+    	return *this;
+    }
+    mint operator+(const mint a) const {
+        mint res(*this);
+        return res+=a;
+    }
+    mint operator-(const mint a) const {
+        mint res(*this);
+        return res-=a;
+    }
+    mint operator*(const mint a) const {
+        mint res(*this);
+        return res*=a;
+    }
+    mint pow(long long t) const {
+        if (!t) return 1;
+        mint a = pow(t>>1);
+        a *= a;
+    	if (t&1) a *= *this;
+        return a;
+    }
+    // for prime MOD
+    mint inv() const {
+        return pow(MOD-2);
+    }
+    mint& operator/=(const mint a) {
+    	return (*this) *= a.inv();
+    }
+    mint operator/(const mint a) const {
+        mint res(*this);
+        return res/=a;
+    }
+};
+
+struct combination {
+    vector<mint> fact, ifact;
+    combination(long long n):fact(n+1),ifact(n+1) {
+        assert(n < MOD);
+        fact[0] = 1;
+        for (int i = 1; i <= n; ++i) fact[i] = fact[i-1]*i;
+        ifact[n] = fact[n].inv();
+        for (int i = n; i >= 1; --i) ifact[i-1] = ifact[i]*i;
+    }
+    mint operator()(long long n, long long k) {
+        if (k < 0 || k > n) return 0;
+        return fact[n]*ifact[k]*ifact[n-k];
+    }
+};
+
+//nの素因数分解する
+map<ll,ll> factorize(ll n){
+    map<ll,ll> ret;
+    ll x = n;
+    for(ll i = 2; i*i<=n;i++){
+        while(x % i == 0){
+            ret[i]++;
+            x /= i;
+        }
+    }
+    if(x != 1)ret[x]++;
+    return ret;
+}
+signed main()
+{   
+    gearup;
+    ll n,m; cin >> n >> m;
+    mint res = 1;
+    auto ans = factorize(m);
+    combination C(n*2+100);
+    for(auto a:ans){
+        ll num = a.second;
+        res *= C(n-1+num,num); //仕切りn-1と玉num個の重複組み合わせ
+    }
+    out(res.x);
+}
