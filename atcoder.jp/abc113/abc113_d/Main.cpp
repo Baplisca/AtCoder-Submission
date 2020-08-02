29111ad5e1fb2224d9bdc958bf0400693a850661
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-08-02 14:32:44 +0900

    [AC] abc113 abc113_d

---
+++ atcoder.jp/abc113/abc113_d/Main.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<n;++i)
+#define exrep(i, a, b) for(ll i = a; i < b; i++)
+#define out(x) cout << x << endl
+#define EPS (1e-7)
+#define gearup ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pair<int,int> > vpii;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef vector<vector<bool> > vvb;
+typedef vector<vector<double> > vvd;
+typedef vector<vector<string> > vvs;
+typedef vector<ll> vl;
+typedef vector<vector<ll> > vvl;
+typedef vector<vector<vector<ll> > > vvvl;
+ll MOD = 1000000007;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+template<class T> void debug(T v){rep(i,v.size()) cout<<v[i]<<" ";cout<<endl;}
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//以降 cin の入力元が 'input.txt' になる
+//std::ifstream in("input.txt");
+//std::cin.rdbuf(in.rdbuf());
+ 
+signed main()
+{
+    gearup;
+    int h,w,k; cin >> h >> w >> k;
+    vvl dp(h+10,vl(w+10));
+    dp[0][0] = 1; //dp[i][j]: iまでの高さでjに行く通り
+    rep(i,h){
+        rep(j,w){
+            rep(bit,1<<(w-1)){ //w-1のbit全探索
+                bool check_bit = true;
+                rep(bi,w-2){
+                    if( ((bit >> bi) & 1) && ((bit >> (bi+1)) & 1)) check_bit = false;
+                }
+                if(check_bit == false)continue;
+                int next = j;
+                if( (bit >> j) & 1)next++;
+                else if( j > 0 && (bit >> (j-1)) & 1)next--;
+                dp[i+1][next] += dp[i][j];
+                dp[i+1][next] %= MOD;
+            }
+        }
+    }
+    out(dp[h][k-1]);
+}
