af84f0650c4179e0a56994c6a0fa628f256ac8f6
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-08-11 12:09:04 +0900

    [AC] arc019 arc019_2

---
+++ atcoder.jp/arc019/arc019_2/Main.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<n;++i)
+#define exrep(i, a, b) for(ll i = a; i < b; i++)
+#define out(x) cout << x << endl
+#define EPS (1e-7)
+#define gearup ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pair<int,int> > vpii;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef vector<vector<bool> > vvb;
+typedef vector<vector<double> > vvd;
+typedef vector<vector<string> > vvs;
+typedef vector<ll> vl;
+typedef vector<vector<ll> > vvl;
+typedef vector<vector<vector<ll> > > vvvl;
+ll MOD = 1000000007;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+template<class T> void debug(T v){rep(i,v.size()) cout<<v[i]<<" ";cout<<endl;}
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//以降 cin の入力元が 'input.txt' になる
+//std::ifstream in("input.txt");
+//std::cin.rdbuf(in.rdbuf());
+
+signed main()
+{
+    gearup;
+    
+    string s; cin >> s;
+    int n = s.size();
+    if(n == 1){
+        out(0);
+        return 0;
+    }
+
+    ll cnt = 0;
+    rep(i,n/2){
+        if(s[i] == s[n-i-1])cnt+=2;
+    }
+
+    ll res = 0;
+    //偶数文字列
+    if(n % 2 == 0){
+        rep(i,n){
+            //すでに回文
+            if(cnt == n)res+=25;
+            else if(cnt == n-2){ //ワンペアだけ回文じゃない
+                int idx = min(i,n-i-1);
+                if(s[idx] != s[n-idx -1])res+=24;
+                else res += 25;
+            }
+            else res += 25;
+        }
+    }
+    else { //奇数
+        rep(i,n){
+            if(i == n/2){
+                if(cnt != n-1)res+=25;
+            }
+            else{
+                //すでに回文
+                if(cnt == n-1)res+=25;
+                else if(cnt == n-3){ //ワンペアだけ回文じゃない
+                    int idx = min(i,n-i-1);
+                    if(s[idx] != s[n-idx -1])res+=24;
+                    else res += 25;
+                }
+                else res += 25;
+            }
+        }
+    }
+    out(res);
+}
