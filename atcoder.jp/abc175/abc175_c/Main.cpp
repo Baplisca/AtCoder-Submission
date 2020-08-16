612a5487848b3cd99be6045a24245fd66d8c6369
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-08-16 17:17:33 +0900

    [AC] abc175 abc175_c

---
+++ atcoder.jp/abc175/abc175_c/Main.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<n;++i)
+#define exrep(i, a, b) for(ll i = a; i < b; i++)
+#define out(x) cout << x << endl
+#define EPS (1e-7)
+#define gearup ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pair<int,int> > vpii;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef vector<vector<bool> > vvb;
+typedef vector<vector<double> > vvd;
+typedef vector<vector<string> > vvs;
+typedef vector<ll> vl;
+typedef vector<vector<ll> > vvl;
+typedef vector<vector<vector<ll> > > vvvl;
+ll MOD = 1000000007;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+template<class T> void debug(T v){rep(i,v.size()) cout<<v[i]<<" ";cout<<endl;}
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//以降 cin の入力元が 'input.txt' になる
+//std::ifstream in("input.txt");
+//std::cin.rdbuf(in.rdbuf());
+
+
+signed main()
+{
+    gearup;
+    ll x,k,d; cin >> x >> k >> d;
+    if(x == 0){
+        if(k % 2 == 0)out(0);
+        else out(d);
+    }
+    else if(x > 0){
+        // x - k*d > 0
+        // x/d > k
+        //切り上げ (x+d-1)/d > k
+        if( (x+d-1)/d > k){
+            out(x-k*d); //この時オーバーフローはないため
+        }
+        else{
+            //res1かres2を振動する
+            ll res1 = x % d;
+            ll res2 = abs(d-res1);
+            ll cnt = (x - res1)/d;
+            if( (cnt ^ k) & 1)out(res2);
+            else out(res1);
+        }
+    }
+    else{
+        // abs(x) - k*d > 0
+        x = abs(x);
+        if( (x+d-1)/d > k){
+            out(x-k*d); //この時オーバーフローはないため
+        }
+        else{
+            //res1かres2を振動する
+            ll res1 = x % d;
+            ll res2 = abs(d-res1);
+            ll cnt = (x - res1)/d;
+            if( (cnt ^ k) & 1)out(res2);
+            else out(res1);
+        }
+    }
+}
