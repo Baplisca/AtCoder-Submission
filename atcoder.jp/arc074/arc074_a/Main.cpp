ce7c7edb4bfdc38f0e19431c413acc87fe42de09
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-10-06 21:59:48 +0900

    [AC] arc074 arc074_a

---
+++ atcoder.jp/arc074/arc074_a/Main.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<n;++i)
+#define exrep(i, a, b) for(ll i = a; i < b; i++)
+#define out(x) cout << x << endl
+#define EPS (1e-7)
+#define gearup ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pair<int,int> > vpii;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef vector<vector<bool> > vvb;
+typedef vector<vector<double> > vvd;
+typedef vector<vector<string> > vvs;
+typedef vector<ll> vl;
+typedef vector<vector<ll> > vvl;
+typedef vector<vector<vector<ll> > > vvvl;
+ll MOD = 1000000007;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+template<class T> void debug(T v){rep(i,v.size()) cout<<v[i]<<" ";cout<<endl;}
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//以降 cin の入力元が 'input.txt' になる
+//std::ifstream in("input.txt");
+//std::cin.rdbuf(in.rdbuf());
+
+signed main()
+{
+    gearup;
+    ll h,w; cin >> h >> w;
+    ll res = L_INF;
+    //2つとも同じ方向
+    rep1(i,h-1){
+        ll a = i * w;
+        ll sub_i = (h - i)/2;
+        ll b = sub_i * w;
+        ll c = (h-i-sub_i) * w;
+        ll ma = max(max(a,b),c);
+        ll mi = min(min(a,b),c);
+        res = min(res,ma-mi);
+    }
+    //横と縦
+    rep1(i,h){
+        ll a = i * w;
+        ll sub_j = w/2;
+        ll b = sub_j * (h-i);
+        ll c = (w-sub_j)*(h-i);
+        ll ma = max(max(a,b),c);
+        ll mi = min(min(a,b),c);
+        res = min(res,ma-mi);
+    }
+    swap(w,h);
+    //2つとも同じ方向
+    rep1(i,h-1){
+        ll a = i * w;
+        ll sub_i = (h - i)/2;
+        ll b = sub_i * w;
+        ll c = (h-i-sub_i) * w;
+        ll ma = max(max(a,b),c);
+        ll mi = min(min(a,b),c);
+        res = min(res,ma-mi);
+    }
+    //横と縦
+    rep1(i,h){
+        ll a = i * w;
+        ll sub_j = w/2;
+        ll b = sub_j * (h-i);
+        ll c = (w-sub_j)*(h-i);
+        ll ma = max(max(a,b),c);
+        ll mi = min(min(a,b),c);
+        res = min(res,ma-mi);
+    }
+    out(res);
+}   
