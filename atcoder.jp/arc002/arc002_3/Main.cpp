623d88bea1f0c040f5ce7ba55a5c11ac0ee44bce
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-11-02 11:51:21 +0900

    [AC] arc002 arc002_3

---
+++ atcoder.jp/arc002/arc002_3/Main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<n;++i)
+#define exrep(i, a, b) for(ll i = a; i < b; i++)
+#define out(x) cout << x << endl
+#define EPS (1e-7)
+#define gearup ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pair<int,int> > vpii;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef vector<vector<bool> > vvb;
+typedef vector<vector<double> > vvd;
+typedef vector<vector<string> > vvs;
+typedef vector<ll> vl;
+typedef vector<vector<ll> > vvl;
+typedef vector<vector<vector<ll> > > vvvl;
+ll MOD = 1000000007;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}return false;}
+template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
+template<class T> void debug(T v){rep(i,v.size()) cout<<v[i]<<" ";cout<<endl;}
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//以降 cin の入力元が 'input.txt' になる
+//std::ifstream in("input.txt");
+//std::cin.rdbuf(in.rdbuf());
+
+signed main() {
+    gearup;
+    int n; cin >> n;
+    string ss; cin >> ss;
+    ll ans = 0;
+    string t[] = {"AA","AB","AX","AY","BA","BB","BX","BY",
+                  "XA","XB","XX","XY","YA","YB","YX","YY"};
+    rep(a,16){
+        exrep(b,a+1,16){
+            ll cnt = 0;
+            auto s = ss;
+            rep(i,n-1){
+                if(s[i] == t[a][0] && s[i+1] == t[a][1])s[i+1]='@',cnt++;
+                else if(s[i] == t[b][0] && s[i+1] == t[b][1])s[i+1]='@',cnt++;
+            }
+            ans = max(ans,cnt);
+            //out(cnt);
+        }
+    }
+    out(n-ans);
+}   
