82d32125b3eff5ef51d724406d4a74faa7eefb32
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-11-23 15:02:58 +0900

    [AC] abc184 abc184_c

---
+++ atcoder.jp/abc184/abc184_c/Main.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define rep1(i, n) for (int i = 1; i < n; ++i)
+#define exrep(i, a, b) for (ll i = a; i < b; i++)
+#define out(x) cout << x << endl
+#define EPS (1e-7)
+#define gearup                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<double>> vvd;
+typedef vector<vector<string>> vvs;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+ll MOD = 1000000007;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template <class T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+void debug(T v)
+{
+    rep(i, v.size()) cout << v[i] << " ";
+    cout << endl;
+}
+ll pcount(ll x) { return __builtin_popcountll(x); }
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//以降 cin の入力元が 'input.txt' になる
+//std::ifstream in("input.txt");
+//std::cin.rdbuf(in.rdbuf());
+
+signed main()
+{
+    gearup;
+    ll r1, c1, r2, c2;
+    cin >> r1 >> c1 >> r2 >> c2;
+    r2 -= r1;
+    c2 -= c1;
+    //第1象限へ
+    r2 = abs(r2);
+    c2 = abs(c2);
+    int ans;
+    int dist1 = abs(r2 - c2);
+    if (r2 == c2 && r2 == 0)
+        ans = 0;
+    else if (r2 == c2 || r2 + c2 <= 3)
+        ans = 1;
+    else if ((r2 + c2) % 2 == 0 || r2 + c2 <= 6 || dist1 <= 3)
+        ans = 2;
+    else
+        ans = 3;
+    out(ans);
+}
