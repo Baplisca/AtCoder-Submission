dd79835d63aa450739536120e155f77c732e041f
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-11-25 18:20:34 +0900

    [AC] abc035 abc035_b

---
+++ atcoder.jp/abc035/abc035_b/Main.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define rep1(i, n) for (int i = 1; i < n; ++i)
+#define exrep(i, a, b) for (ll i = a; i < b; i++)
+#define out(x) cout << x << endl
+#define EPS (1e-7)
+#define gearup                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<double>> vvd;
+typedef vector<vector<string>> vvs;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+ll MOD = 1000000007;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template <class T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+void debug(T v)
+{
+    rep(i, v.size()) cout << v[i] << " ";
+    cout << endl;
+}
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//以降 cin の入力元が 'input.txt' になる
+//std::ifstream in("input.txt");
+//std::cin.rdbuf(in.rdbuf());
+
+signed main()
+{
+    gearup;
+    string s;
+    int t;
+    cin >> s >> t;
+    int x = 0, y = 0, hatena_cnt = 0;
+    rep(i, s.size())
+    {
+        if (s[i] == 'L')
+            x--;
+        else if (s[i] == 'R')
+            x++;
+        else if (s[i] == 'U')
+            y++;
+        else if (s[i] == 'D')
+            y--;
+        else
+            hatena_cnt++;
+    }
+    int now = abs(x) + abs(y);
+    if (t == 1)
+        out(now + hatena_cnt);
+    else
+    {
+        if (now > hatena_cnt)
+            out(now - hatena_cnt);
+        else if (abs(now - hatena_cnt) % 2 == 0)
+            out(0);
+        else
+            out(1);
+    }
+}
