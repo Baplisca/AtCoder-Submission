dae3af66fccf55a43ecc28e996653568e1dc3ae9
Baplisca <65814732+Baplisca@users.noreply.github.com>
2020-12-11 19:12:21 +0900

    [AC] abc081 arc086_b

---
+++ atcoder.jp/abc081/arc086_b/Main.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define rep1(i, n) for (int i = 1; i < n; ++i)
+#define exrep(i, a, b) for (ll i = a; i < b; i++)
+#define out(x) cout << x << endl
+#define EPS (1e-7)
+#define gearup                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<double>> vvd;
+typedef vector<vector<string>> vvs;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+ll MOD = 1000000007;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template <class T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+void debug(T v)
+{
+    rep(i, v.size()) cout << v[i] << " ";
+    cout << endl;
+}
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//以降 cin の入力元が 'input.txt' になる
+//std::ifstream in("input.txt");
+//std::cin.rdbuf(in.rdbuf());
+
+signed main()
+{
+    gearup;
+    int n;
+    cin >> n;
+    vl a(n);
+    rep(i, n) cin >> a[i];
+    vpii p;
+    //while (1)
+    {
+        ll ma = -L_INF;
+        ll mi = L_INF;
+        int ma_idx, mi_idx;
+        rep(i, n)
+        {
+            if (chmax(ma, a[i]))
+                ma_idx = i;
+            if (chmin(mi, a[i]))
+                mi_idx = i;
+        }
+        if (abs(ma) >= abs(mi))
+        {
+            rep(i, n) p.push_back({ma_idx + 1, i + 1});
+            rep1(i, n) p.push_back({i, i + 1});
+        }
+        else
+        {
+            rep(i, n) p.push_back({mi_idx + 1, i + 1});
+            for (int i = n - 1; i > 0; i--)
+                p.push_back({i + 1, i});
+        }
+    }
+    out(p.size());
+    rep(i, p.size()) cout << p[i].first << " " << p[i].second << endl;
+    //debug(a);
+}
