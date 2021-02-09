2db2359ae3774eab528f9eb4b5022e4e722908de
Baplisca <65814732+Baplisca@users.noreply.github.com>
2021-02-09 21:09:24 +0900

    [AC] s8pc-4 s8pc_4_b

---
+++ atcoder.jp/s8pc-4/s8pc_4_b/Main.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define rep1(i, n) for (int i = 1; i < n; ++i)
+#define exrep(i, a, b) for (ll i = a; i < b; i++)
+#define out(x) cout << x << endl
+#define EPS (1e-7)
+#define gearup                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<double>> vvd;
+typedef vector<vector<string>> vvs;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+ll MOD = 998244353;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template <class T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+void debug(T v)
+{
+    rep(i, v.size()) cout << v[i] << " ";
+    cout << endl;
+}
+ll pcount(ll x) { return __builtin_popcountll(x); }
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//以降 cin の入力元が 'input.txt' になる
+//std::ifstream in("input.txt");
+//std::cin.rdbuf(in.rdbuf());
+
+signed main()
+{
+    gearup;
+    int n, k;
+    cin >> n >> k;
+    vl a(n);
+    rep(i, n) cin >> a[i];
+    //どのビルを大きくするかbit全探索
+    ll res = L_INF;
+    rep(i, 1 << n)
+    {
+        auto b = a;
+        ll cost = 0;
+        ll ma = b[0];
+        rep1(j, n)
+        {
+            if ((i >> j) & 1)
+            {
+                cost += max(ma + 1, b[j]) - b[j];
+                b[j] = max(ma + 1, b[j]);
+            }
+            ma = max(ma, b[j]);
+        }
+        ll cnt = 1;
+        ma = b[0];
+        rep1(j, n)
+        {
+            if (b[j] > ma)
+                cnt++;
+            ma = max(b[j], ma);
+        }
+        if (cnt >= k)
+            res = min(res, cost);
+    }
+    out(res);
+}
