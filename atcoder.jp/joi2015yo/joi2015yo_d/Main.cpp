a20878c6171216aa2af31e208954f181b4b3828d
Baplisca <65814732+Baplisca@users.noreply.github.com>
2021-02-19 17:16:46 +0900

    [AC] joi2015yo joi2015yo_d

---
+++ atcoder.jp/joi2015yo/joi2015yo_d/Main.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define rep1(i, n) for (int i = 1; i < n; ++i)
+#define exrep(i, a, b) for (ll i = a; i < b; i++)
+#define out(x) cout << x << endl
+#define EPS (1e-7)
+#define gearup                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<double>> vvd;
+typedef vector<vector<string>> vvs;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+ll MOD = 998244353;
+const long long L_INF = 1LL << 60;
+const int INF = 2147483647; // 2^31-1
+const double PI = acos(-1);
+//cout<<fixed<<setprecision(10);
+
+template <class T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+void debug(T v)
+{
+    rep(i, v.size()) cout << v[i] << " ";
+    cout << endl;
+}
+ll pcount(ll x) { return __builtin_popcountll(x); }
+const ll dx[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//以降 cin の入力元が 'input.txt' になる
+//std::ifstream in("input.txt");
+//std::cin.rdbuf(in.rdbuf());
+
+signed main()
+{
+    gearup;
+    int n, m; //nが都市(0 ~ n)、mが日数
+    cin >> n >> m;
+    vl d(n), c(m); //dは距離、cはその日の天候の悪さ
+    rep(i, n) cin >> d[i];
+    rep(i, m) cin >> c[i];
+    vvl dp(m + 1, vl(n + 1, L_INF)); // dp[i][j] : i日目までに都市jを訪れる最小の疲労度
+    dp[0][0] = 0;
+    rep(i, m) rep(j, n)
+    {
+        chmin(dp[i + 1][j], dp[i][j]);                   //待機
+        chmin(dp[i + 1][j + 1], dp[i][j] + c[i] * d[j]); //移動
+    }
+    ll res = L_INF;
+    rep(i, m) chmin(res, dp[i + 1][n]);
+    out(res);
+}
