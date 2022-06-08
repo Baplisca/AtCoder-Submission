4fae157775b650dc10490d54dd6d6f4e7aca9e0d
Baplisca <yokotako0327-goki@outlook.jp>
2022-06-09 07:58:54 +0900

    [AC] code-festival-2014-quala code_festival_qualA_a

---
+++ atcoder.jp/code-festival-2014-quala/code_festival_qualA_a/Main.rs
@@ -0,0 +1,10 @@
+use proconio::{input, marker::Chars};
+use std::{cmp, i32::MAX, io::Chain, string, usize};
+
+fn main() {
+    input! {
+        s:String
+    }
+    let res = s + "2014";
+    println!("{}", res);
+}
