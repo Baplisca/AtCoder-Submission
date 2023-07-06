ddda3ea2b87be221895b7578d25c3b3ca1a277a9
Baplisca <yokotako0327-goki@outlook.jp>
2023-07-06 18:27:15 +0900

    [AC] abc297 abc297_c

---
+++ atcoder.jp/abc297/abc297_c/Main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+signed main()
+{
+    int h, w;
+    cin >> h >> w;
+    vector<string> s(h);
+    for (int i = 0; i < h; i++)
+    {
+        cin >> s[i];
+    }
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w - 1; j++)
+        {
+            if (s[i][j] == s[i][j + 1] && s[i][j] == 'T')
+            {
+                s[i][j] = 'P', s[i][j + 1] = 'C';
+            }
+        }
+    }
+    for (int i = 0; i < h; i++)
+    {
+        cout << s[i] << endl;
+    }
+}
