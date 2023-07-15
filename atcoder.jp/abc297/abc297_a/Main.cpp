c7f19b0fd38b3bf49d6d441eba50100e724e4c95
Baplisca <yokotako0327-goki@outlook.jp>
2023-07-15 22:31:33 +0900

    [AC] abc297 abc297_a

---
+++ atcoder.jp/abc297/abc297_a/Main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+signed main()
+{
+    int n, d;
+    cin >> n >> d;
+    vector<int> t(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> t[i];
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (t[i + 1] - t[i] <= d)
+        {
+            cout << t[i + 1] << endl;
+            return 0;
+        }
+    }
+    cout << -1 << endl;
+}
